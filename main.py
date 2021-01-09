2df5b027a305e697f50802dfd70857065e1edaae
yairgur <yair.gur.arieh@gmail.com>
2021-01-10 01:30:05 +0200

    commit after all parsing works

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import linecache
 import statistics
+from collections import Counter
 from scipy import sparse
 
 ATTR_NUM = 8    # Number of attributes for each vote in the input file
 file = open("wiki-RfA.txt", "r")
-G = nx.DiGraph()
-NDG = nx.Graph()
+
 
 
 class Vote(object):
@@ -71,13 +71,29 @@ def create_votes_lst():
 
 
 """ Create a list of voters with all info, only voters which voted more than "treshold" times """
-def create_voters_lst(votes_lst, distinct_voter_lst, treshold):
-    i = 0
+def create_voters_lst(votes_lst, treshold):
+    iter = 0
     total_len = 0
     total_votes = 0
     res = []
     comment_lst = []
+    votes = Counter(vote.voter for vote in votes_lst)
     for vote in votes_lst:
+        total_len = total_len + len(vote.comment)
+        comment_lst.append((vote.choice, vote.comment, vote.voted_for))
+        total_votes = total_votes+1
+        iter = iter+1
+        if iter==votes[vote.voter]:
+            if total_votes > treshold:
+                res.append(Voter(vote.voter, total_votes, total_len // total_votes, 0, 0, 0, 0, comment_lst))
+            iter=0
+            comment_lst = []
+            total_len = 0
+            total_votes = 0
+
+
+
+    """for vote in votes_lst:
         if vote.voter == distinct_voter_lst[i]:
             total_votes=total_votes+1
             total_len=total_len+len(vote.comment)
@@ -89,6 +105,7 @@ def create_voters_lst(votes_lst, distinct_voter_lst, treshold):
             i=i+1
             total_len=len(vote.comment)
             total_votes=0
+            """
     update_votes_for_voters(res)
     update_is_elected(votes_lst, res)
     return res
@@ -191,41 +208,48 @@ def corolation(voters_lst, median):
 
 
 """ Graph analysis and creation """
-def create_graph(votes_lst, distinct_voters_lst, treshold):
-    i = 0
-    edges = []
-    voted_lst = []
-    for vote in votes_lst:
-        if vote.voter == distinct_voters_lst[i]:
-            voted_lst.append(vote.voted_for)
-        else:
-            edges.append((vote.voter, vote.voted_for, vote.choice, len(voted_lst)))
-            voted_lst = [vote.voted_for]
-            i = i+1
-    for edge in edges:
-        if edge[3] > treshold:
-            if edge[2] == "1":
-                    G.add_edge(edge[0], edge[1], color='b', weight=1, relation="support")
-                    NDG.add_edge(edge[0], edge[1], color='b', weight=1, relation="support")
-            elif edge[2] == "-1":
-                    G.add_edge(edge[0], edge[1], color='r', weight=-1, relation="oppose")
-                    NDG.add_edge(edge[0], edge[1], color='r', weight=-1, relation="oppose")
+def create_graph(G, NDG, voters_lst, treshold):
+    #votes = Counter(vote.voted_for for vote in votes_lst)
+    for voter in voters_lst:
+        if voter.total_votes > treshold and voter.total_votes < 50:
+            for comment in voter.comment_lst:
+                if comment[0] == "1":
+                    G.add_edge(voter.name, comment[2], color='b', weight=1, relation="support")
+                    NDG.add_edge(voter.name, comment[2], color='b', weight=1, relation="support")
+                elif comment[0] == "-1":
+                    G.add_edge(voter.name, comment[2], color='r', weight=-1, relation="oppose")
+                    NDG.add_edge(voter.name, comment[2], color='r', weight=-1, relation="oppose")
+                else:
+                    G.add_edge(voter.name, comment[2], color='y', weight=0, relation="neutral")
+                    NDG.add_edge(voter.name, comment[2], color='y', weight=0, relation="neutral")
+
+    """for vote in votes_lst:
+        if int(votes[vote.voted_for]) > treshold:
+            print("yair     ", votes[vote.voted_for])
+            if vote.choice == "1":
+                G.add_edge(vote.voter, vote.voted_for, color='b', weight=1, relation="support")
+                NDG.add_edge(vote.voter, vote.voted_for, color='b', weight=1, relation="support")
+            elif vote.choice == "-1":
+                G.add_edge(vote.voter, vote.voted_for, color='r', weight=-1, relation="oppose")
+                NDG.add_edge(vote.voter, vote.voted_for, color='r', weight=-1, relation="oppose")
             else:
-                G.add_edge(edge[0], edge[1], color='y', weight=0, relation="neutral")
-                NDG.add_edge(edge[0], edge[1], color='y', weight=0, relation="neutral")
+                G.add_edge(vote.voter, vote.voted_for, color='y', weight=0, relation="neutral")
+                NDG.add_edge(vote.voter, vote.voted_for, color='y', weight=0, relation="neutral")"""
 
 
 def plot_deg_dist(G):
-    degrees = [G.degree(n) for n in G.nodes()]
-    plt.hist(degrees)
-    # all_degrees = nx.degree(G).values()
-    # unique_deg = f7(all_degrees)
-    # count_of_degrees = []
-    # for deg in unique_deg:
-     #    x = all_degrees.count(deg)
-     #    count_of_degrees.append(x)
+    #degrees = [G.degree(n) for n in G.nodes()]
+    #plt.hist(degrees)
+    all_degrees = [val for (node, val) in G.degree()]
+    #all_degrees = nx.degree(G)
+    #print("yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy", all_degrees, "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy")
+    unique_deg = f7(all_degrees)
+    count_of_degrees = []
+    for deg in unique_deg:
+        x = all_degrees.count(deg)
+        count_of_degrees.append(x)
 
-    # plt.plot(unique_deg, count_of_degrees, 'yo-')
+    plt.plot(unique_deg, count_of_degrees, 'yo-')
     plt.show()
 
 
@@ -272,11 +296,10 @@ def deg_dist(G):
     out_degree_freq = degree_histogram_directed(G, out_degree=True)
     degrees = range(len(in_degree_freq))
     plt.figure(figsize=(12, 8))
-    # Demonstrating power-law
-    plt.loglog(range(len(in_degree_freq)), in_degree_freq, 'go-', label='in-degree')
-    plt.loglog(range(len(out_degree_freq)), out_degree_freq, 'bo-', label='out-degree')
-    plt.xlabel('Degrees')
-    plt.ylabel('Number of nodes')
+    plt.plot(range(len(in_degree_freq)), in_degree_freq, 'go-', label='in-degree')
+    plt.plot(range(len(out_degree_freq)), out_degree_freq, 'bo-', label='out-degree')
+    plt.xlabel('Degree')
+    plt.ylabel('Frequency')
     plt.title("Degree distribution of RfA")
 
 
@@ -296,27 +319,30 @@ def find_max_connected_components(G):
 
 
 def main():
-    treshold = 1
+
+    G = nx.DiGraph()
+    NDG = nx.Graph()
+    treshold = 0
     votes_lst = create_votes_lst()
-    distinct_voter_lst = distinct_voters_lst(votes_lst)
-    voters_lst = create_voters_lst(votes_lst, distinct_voter_lst, treshold)
-    create_graph(votes_lst, distinct_voter_lst, treshold)
+    #distinct_voter_lst = distinct_voters_lst(votes_lst)
+    voters_lst = create_voters_lst(votes_lst, treshold)
+    print("how many votes?", len(votes_lst))
+    create_graph(G, NDG, voters_lst, treshold)
+    print("how many voters? ", len(Counter(vote.voter for vote in votes_lst)))
     # relation = nx.get_edge_attributes(G, "relation")
     colors = nx.get_edge_attributes(G, 'color').values()
     pos = nx.spring_layout(G)   # pos = nx.circular_layout(G)
     deg = dict(G.degree)
-    """
+
     nx.draw(G, pos,
             edge_color=colors,
-            node_size=[v * 10 for v in deg.values()])
+            node_size=[v / 10 for v in deg.values()])
 
-    """
+
     """,with_labels=True)"""
     plt.show()
 
-
-
-    plot_deg_dist(G)
+    plot_deg_dist(NDG)
     deg_dist(G)
     plt.show()
 
@@ -324,15 +350,21 @@ def main():
         #print(i.name, i.oppose_votes, i.neutral_votes, i.support_votes, i.total_votes, i.avt_vote_len, i.is_elected)
     #for i in corolation(voters_lst, get_median_of_votes_length(votes_lst)):
         #print(i[0], i[1], i[2])
-    print(len(voters_lst))
+    #print("there are: ", len(voters_lst), " voters")
+    #print("there are: ", len(distinct_voted_for_lst(votes_lst)), " votees")
     print(nx.info(G))
-    print("Out of: %d voters, %d has a positive views in more than 80 precents of their votes.\n total of %f precents"
-          % (len(voters_lst),  len(positive_voters(voters_lst, 0.8)), (len(positive_voters(voters_lst, 0.8))/len(voters_lst))))
-    print("Out of: %d voters, %d has a negative views in more than 80 precents of their votes.\n total of %f precents"
-          % (len(voters_lst), len(negative_voters(voters_lst, 0.8)),
-             (len(negative_voters(voters_lst, 0.8)) / len(voters_lst))))
-    print(nx.density(NDG))
-    #show_clustering(NDG)
+    #for voter in voters_lst:
+    #    if(voter.total_votes > treshold):
+    #        print(voter.name, voter.total_votes, voter.is_elected)
+
+#    votes = Counter(vote.voter for vote in votes_lst)
+    #print("Out of: %d voters, %d has a positive views in more than 80 precents of their votes.\n total of %f precents"
+    #      % (len(voters_lst),  len(positive_voters(voters_lst, 0.8)), (len(positive_voters(voters_lst, 0.8))/len(voters_lst))))
+    #print("Out of: %d voters, %d has a negative views in more than 80 precents of their votes.\n total of %f precents"
+    #      % (len(voters_lst), len(negative_voters(voters_lst, 0.8)),
+    #         (len(negative_voters(voters_lst, 0.8)) / len(voters_lst))))
+    #print(nx.density(NDG))
+    show_clustering(NDG)
     print(nx.diameter(NDG))
 
 
